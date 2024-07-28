78c6e0ffbc5f218c4c5714c88bdd8332a0b72356
Shiva Pasunuri <developers@momentumventures.io>
2024-07-28 23:50:31 +0530

    added the links

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -12,6 +12,7 @@ import {
   Utensils,
 } from "lucide-react";
 import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
 
 const Navbar = () => {
   const { data, status } = useSession();
@@ -64,97 +65,44 @@ const Navbar = () => {
           <div className="py-4 overflow-y-auto">
             <ul className="space-y-2 font-medium">
               <li>
-                <a
-                  href="#"
+                <Link
+                  href="/"
                   className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
                 >
                   <Home size={20} />
                   <span className="ms-3">Home</span>
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href="#"
+                <Link
+                  href="/menu"
                   className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
                 >
                   <Utensils size={20} />
                   <span className="ms-3">Menu</span>
-                </a>
+                </Link>
               </li>
-              {/* <li>
-                <button
-                  type="button"
-                  className="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                  aria-controls="dropdown-example"
-                  data-collapse-toggle="dropdown-example"
-                >
-                  <Utensils size={20} />
-                  <span className="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">
-                    Menu
-                  </span>
-                  <svg
-                    className="w-3 h-3"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 10 6"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="m1 1 4 4 4-4"
-                    />
-                  </svg>
-                </button>
-                <ul id="dropdown-example" className="hidden py-2 space-y-2">
-                  <li>
-                    <a
-                      href="#"
-                      className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                    >
-                      Ramens
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                    >
-                      Bowls
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                    >
-                      Burgers
-                    </a>
-                  </li>
-                </ul>
-              </li> */}
+
               <li>
-                <a
+                <Link
                   href="#"
                   className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
                 >
                   <Pizza size={20} />
                   <span className="flex-1 ms-3 whitespace-nowrap">Order</span>
-                </a>
+                </Link>
               </li>
               <li>
-                <a
+                <Link
                   href="#"
                   className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
                 >
                   <Star size={20} />
                   <span className="flex-1 ms-3 whitespace-nowrap">Blogs</span>
-                </a>
+                </Link>
               </li>
               <li>
-                <a
+                <Link
                   href="#"
                   className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
                 >
@@ -162,7 +110,7 @@ const Navbar = () => {
                   <span className="flex-1 ms-3 whitespace-nowrap">
                     Testimonals
                   </span>
-                </a>
+                </Link>
               </li>
               <li>
                 <a
