a65a8242c098c869720208478379e07cc6818b0b
Shiva Pasunuri <developers@momentumventures.io>
2024-08-04 22:59:17 +0530

    added few changes

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import {
   Home,
+  LogOut,
   Menu,
   MessageSquareCode,
   Phone,
@@ -16,6 +17,7 @@ import Link from "next/link";
 
 const Navbar = () => {
   const { data, status } = useSession();
+  // console.log(data);
   return (
     <div className="w-full px-2 flex md:grid md:grid-cols-4 md:px-6 py-3">
       <div className="w-full flex items-center justify-start sm:hidden">
@@ -125,6 +127,17 @@ const Navbar = () => {
                   </span>
                 </a>
               </li>
+              <li>
+                <a
+                  type="button"
+                  className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+                >
+                  <LogOut size={20} />
+                  <span className="flex-1 ms-3 whitespace-nowrap">
+                    Sign Out
+                  </span>
+                </a>
+              </li>
             </ul>
           </div>
         </div>
@@ -160,12 +173,12 @@ const Navbar = () => {
             <button
               id="hs-dropdown-default"
               type="button"
-              className="hs-dropdown-toggle py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
+              className="hs-dropdown-toggle p-2 inline-flex items-center gap-x-2 text-sm rounded-md border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
               aria-haspopup="menu"
               aria-expanded="false"
               aria-label="Dropdown"
             >
-              {data.user?.name}
+              {data.user?.name?.split(" ")[0]}
               <svg
                 className="hs-dropdown-open:rotate-180 size-4"
                 xmlns="http://www.w3.org/2000/svg"
@@ -183,35 +196,57 @@ const Navbar = () => {
             </button>
 
             <div
-              className="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-60 bg-white shadow-md rounded-lg p-1 space-y-0.5 mt-2 after:h-4 after:absolute after:-bottom-4 after:start-0 after:w-full before:h-4 before:absolute before:-top-4 before:start-0 before:w-full"
+              className="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-32 max-w-52 bg-white shadow-md rounded-lg p-1 space-y-0.5 mt-2 after:h-4 after:absolute after:-bottom-4 after:start-0 after:w-full before:h-4 before:absolute before:-top-4 before:start-0 before:w-full"
               role="menu"
               aria-orientation="vertical"
               aria-labelledby="hs-dropdown-default"
             >
-              <a
-                className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
-                type="button"
+              <div className="px-4 py-3 text-sm text-gray-900">
+                <div>{data.user?.name}</div>
+                <div className="font-medium truncate">{data.user?.email}</div>
+              </div>
+              <hr />
+              <ul
+                className="py-2 text-sm text-gray-700 "
+                aria-labelledby="dropdownUserAvatarButton"
               >
-                Newsletter
-              </a>
-              <a
-                className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
-                type="button"
-              >
-                Purchases
-              </a>
-              <a
-                className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
-                type="button"
-              >
-                Downloads
-              </a>
-              <a
-                className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
-                type="button"
-              >
-                Team Account
-              </a>
+                <li>
+                  <a
+                    type="button"
+                    className="block px-2 py-1 hover:bg-gray-100 "
+                  >
+                    Your Orders
+                  </a>
+                </li>
+                <li>
+                  <a
+                    type="button"
+                    className="block px-2 py-1 hover:bg-gray-100 "
+                  >
+                    Settings
+                  </a>
+                </li>
+                <li>
+                  <a
+                    type="button"
+                    className="block px-2 py-1 hover:bg-gray-100 "
+                  >
+                    Refer
+                  </a>
+                </li>
+              </ul>
+              <hr />
+              <div className="py-2">
+                <a
+                  onClick={() => {
+                    signOut();
+                  }}
+                  type="button"
+                  className="block px-2 py-1 text-sm text-gray-700 hover:bg-gray-100 "
+                >
+                  Sign out
+                </a>
+              </div>
             </div>
           </div>
         ) : (
@@ -219,19 +254,39 @@ const Navbar = () => {
             onClick={() => {
               signIn("google");
             }}
-            className="flex items-center justify-center h-10 w-10 bg-black text-white rounded-full"
+            className="flex items-center justify-center space-x-2 bg-black text-white rounded-md p-2 md:p-2 lg:p-3 "
           >
-            <User size={15} />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              width="25"
+              height="25"
+              viewBox="0 0 48 48"
+            >
+              <path
+                fill="#fbc02d"
+                d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12	s5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24s8.955,20,20,20	s20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
+              ></path>
+              <path
+                fill="#e53935"
+                d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039	l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
+              ></path>
+              <path
+                fill="#4caf50"
+                d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36	c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
+              ></path>
+              <path
+                fill="#1565c0"
+                d="M43.611,20.083L43.595,20L42,20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571	c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
+              ></path>
+            </svg>
+            <p className="text-xs">Continue With Google</p>
           </button>
         )}
       </div>
-      <div className="hidden justify-end items-center md:flex md:space-x-4 md:col-span-1">
-        {/* <button className="flex items-center justify-center bg-black text-white rounded-full md:p-2 lg:p-3 ">
-          <Search size={20} />
-        </button>
-        <button className="flex items-center justify-center bg-black text-white rounded-full md:p-2 lg:p-3 ">
-          <ShoppingCart size={20} />
-        </button> */}
+
+      <div className="hidden web justify-end items-center md:flex md:space-x-4 md:col-span-1">
         {status === "loading" ? (
           <div
             className="animate-spin inline-block size-6 border-[3px] border-current border-t-transparent text-blue-600 rounded-full "
@@ -268,35 +323,57 @@ const Navbar = () => {
             </button>
 
             <div
-              className="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-60 bg-white shadow-md rounded-lg p-1 space-y-0.5 mt-2 after:h-4 after:absolute after:-bottom-4 after:start-0 after:w-full before:h-4 before:absolute before:-top-4 before:start-0 before:w-full"
+              className="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-60 max-w-60 bg-white shadow-md rounded-lg p-1 space-y-0.5 mt-2 after:h-4 after:absolute after:-bottom-4 after:start-0 after:w-full before:h-4 before:absolute before:-top-4 before:start-0 before:w-full"
               role="menu"
               aria-orientation="vertical"
               aria-labelledby="hs-dropdown-default"
             >
-              <a
-                className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
-                type="button"
+              <div className="px-4 py-3 text-sm text-gray-900">
+                <div>{data.user?.name}</div>
+                <div className="font-medium truncate">{data.user?.email}</div>
+              </div>
+              <hr />
+              <ul
+                className="py-2 text-sm text-gray-700 "
+                aria-labelledby="dropdownUserAvatarButton"
               >
-                Newsletter
-              </a>
-              <a
-                className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
-                type="button"
-              >
-                Purchases
-              </a>
-              <a
-                className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
-                type="button"
-              >
-                Downloads
-              </a>
-              <a
-                className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
-                type="button"
-              >
-                Team Account
-              </a>
+                <li>
+                  <a
+                    type="button"
+                    className="block px-2 py-2 hover:bg-gray-100 rounded-md cursor-pointer"
+                  >
+                    Your Orders
+                  </a>
+                </li>
+                <li>
+                  <a
+                    type="button"
+                    className="block px-2 py-2 hover:bg-gray-100 rounded-md cursor-pointer"
+                  >
+                    Settings
+                  </a>
+                </li>
+                <li>
+                  <a
+                    type="button"
+                    className="block px-2 py-2 hover:bg-gray-100 rounded-md cursor-pointer"
+                  >
+                    Refer
+                  </a>
+                </li>
+              </ul>
+              <hr />
+              <div className="py-2 px-2 cursor-pointer">
+                <a
+                  onClick={() => {
+                    signOut();
+                  }}
+                  type="button"
+                  className="block px-2 py-1 text-sm text-gray-700 cursor-pointer hover:bg-gray-100"
+                >
+                  Sign out
+                </a>
+              </div>
             </div>
           </div>
         ) : (
@@ -304,9 +381,34 @@ const Navbar = () => {
             onClick={() => {
               signIn("google");
             }}
-            className="flex items-center justify-center bg-black text-white rounded-full md:p-2 lg:p-3 "
+            className="flex items-center justify-center space-x-2 bg-black text-white rounded-md md:p-2 lg:p-3 "
           >
-            <User size={20} />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              width="25"
+              height="25"
+              viewBox="0 0 48 48"
+            >
+              <path
+                fill="#fbc02d"
+                d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12	s5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24s8.955,20,20,20	s20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
+              ></path>
+              <path
+                fill="#e53935"
+                d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039	l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
+              ></path>
+              <path
+                fill="#4caf50"
+                d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36	c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
+              ></path>
+              <path
+                fill="#1565c0"
+                d="M43.611,20.083L43.595,20L42,20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571	c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
+              ></path>
+            </svg>
+            <p className="md:text-xs">Continue With Google</p>
           </button>
         )}
       </div>
