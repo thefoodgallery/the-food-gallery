e7fc1a330b636962f275f9e17706be06ebf97c06
Shiva Pasunuri <developers@momentumventures.io>
2024-08-04 22:01:38 +0530

    fixed the footer dark colors

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -7,7 +7,7 @@ import YoutubeIcon from "@/public/svg/youtube-circle-logo-svgrepo-com.svg";
 
 const PageFooter = () => {
   return (
-    <footer className="bg-white dark:bg-gray-900">
+    <footer className="bg-white">
       <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
@@ -17,17 +17,17 @@ const PageFooter = () => {
                 className="h-10 me-3"
                 alt="Logo"
               />
-              <span className="self-center text-2xl md:text-xl font-semibold whitespace-nowrap dark:text-white">
+              <span className="self-center text-2xl md:text-xl font-semibold whitespace-nowrap">
                 The Food Gallery
               </span>
             </a>
           </div>
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
             <div>
-              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
                 Resources
               </h2>
-              <ul className="text-gray-500 dark:text-gray-400 font-medium">
+              <ul className="text-gray-500 font-medium">
                 <li className="mb-4">
                   <a href="#" className="hover:underline">
                     Menu
@@ -41,10 +41,10 @@ const PageFooter = () => {
               </ul>
             </div>
             <div>
-              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
                 Follow us
               </h2>
-              <ul className="text-gray-500 dark:text-gray-400 font-medium">
+              <ul className="text-gray-500 font-medium">
                 <li className="mb-4">
                   <a href="#" className="hover:underline ">
                     Youtube
@@ -58,10 +58,10 @@ const PageFooter = () => {
               </ul>
             </div>
             <div>
-              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
                 About
               </h2>
-              <ul className="text-gray-500 dark:text-gray-400 font-medium">
+              <ul className="text-gray-500 font-medium">
                 <li className="mb-4">
                   <a href="#" className="hover:underline">
                     Testimonals
@@ -76,9 +76,9 @@ const PageFooter = () => {
             </div>
           </div>
         </div>
-        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
         <div className="sm:flex sm:items-center sm:justify-between">
-          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          <span className="text-sm text-gray-500 sm:text-center">
             © 2024{" "}
             <a href="#" className="hover:underline">
               The Food Gallery™
@@ -86,23 +86,20 @@ const PageFooter = () => {
             . All Rights Reserved.
           </span>
           <div className="flex mt-4 sm:justify-center sm:mt-0">
-            <a
-              href="#"
-              className="text-gray-500 hover:text-gray-900 dark:hover:text-white h-5 w-5"
-            >
+            <a href="#" className="text-gray-500 hover:text-gray-900 h-5 w-5">
               <FaceBookIcon />
               <span className="sr-only">Facebook page</span>
             </a>
             <a
               href="#"
-              className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5 h-5 w-5"
+              className="text-gray-500 hover:text-gray-900 ms-5 h-5 w-5"
             >
               <InstagramIcon />
               <span className="sr-only">Instagram</span>
             </a>
             <a
               href="#"
-              className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5 h-5 w-5"
+              className="text-gray-500 hover:text-gray-900 ms-5 h-5 w-5"
             >
               <TwitterIcon />
 
@@ -110,7 +107,7 @@ const PageFooter = () => {
             </a>
             <a
               href="#"
-              className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5 h-5 w-5"
+              className="text-gray-500 hover:text-gray-900 ms-5 h-5 w-5"
             >
               <YoutubeIcon />
               <span className="sr-only">GitHub account</span>
