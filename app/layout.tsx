b345a513fe6478ef884454f3c7047ce32f5ffb54
Shiva Pasunuri <developers@momentumventures.io>
2024-08-04 16:11:43 +0530

    added all inits

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,20 @@ export default function RootLayout({
   useEffect(() => {
     if (typeof window !== "undefined") {
       import("flowbite").then((module) => module.initFlowbite());
+      import("flowbite").then((module) => module.initAccordions());
+      import("flowbite").then((module) => module.initCarousels());
+      import("flowbite").then((module) => module.initCollapses());
+      import("flowbite").then((module) => module.initDials());
+      import("flowbite").then((module) => module.initDismisses());
+      import("flowbite").then((module) => module.initDrawers());
+      import("flowbite").then((module) => module.initDropdowns());
+      import("flowbite").then((module) => module.initModals());
+      import("flowbite").then((module) => module.initPopovers());
+      import("flowbite").then((module) => module.initTabs());
+      import("flowbite").then((module) => module.initTooltips());
+      import("flowbite").then((module) => module.initInputCounters());
+      import("flowbite").then((module) => module.initCopyClipboards());
+      import("flowbite").then((module) => module.initDatepickers());
     }
   }, []);
 
