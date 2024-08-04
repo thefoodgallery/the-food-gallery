4c9ae71a72a5f2056953bfe834c0decf784d0f12
Shiva Pasunuri <developers@momentumventures.io>
2024-08-04 18:00:05 +0530

    changed the profile picture back

--- app/menu/layout.tsx
+++ app/menu/layout.tsx
@@ -9,20 +9,6 @@ export default function MenuPageLayour({
   return (
     <main>
       {children}
-      <div className="text-center">
-        <button
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-          type="button"
-          data-drawer-target="drawer-swipe"
-          data-drawer-show="drawer-swipe"
-          data-drawer-placement="bottom"
-          data-drawer-edge="true"
-          data-drawer-edge-offset="bottom-[60px]"
-          aria-controls="drawer-swipe"
-        >
-          Show swipeable drawer
-        </button>
-      </div>
       <div
         id="drawer-swipe"
         className="fixed z-40 w-full overflow-y-auto bg-white border-t border-gray-200 rounded-t-lg dark:border-gray-700 dark:bg-gray-800 transition-transform bottom-0 left-0 right-0 translate-y-full bottom-[60px]"
